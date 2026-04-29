97b4a382ae5d6649b33f346e244ef7659efbbcf7
Ilya <ilyar@example.com>
2026-04-29 20:15:11 +0300

    feat: register HeroDataUpdateHostedService in DI

--- Dota2MetaChecker.TelegramBot/Program.cs
+++ Dota2MetaChecker.TelegramBot/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Repository;
 using Repository.Contracts;
+using Services.Contracts.Data_sync;
 using Services.Contracts.Deserialization;
 using Services.Contracts.Formatting;
 using Services.Contracts.Processing;
@@ -25,11 +26,11 @@ builder.Services.AddDbContext<DatabaseContext>(options =>
 builder.Services.AddSingleton<HeroesDataCache>();
 
 // Регистрация API клиента через HttpClient 
-builder.Services.AddSingleton<ITelegramBotClient>(sp => 
+builder.Services.AddSingleton<ITelegramBotClient>(sp =>
     new TelegramBotClient(builder.Configuration["Telegram:Token"] ?? throw new Exception("Token not found")));
 
 // Регистрация бизнес-логики
-builder.Services.AddSingleton<IStratzApiService>(sp => 
+builder.Services.AddSingleton<IStratzApiService>(sp =>
     new StratzApiService(builder.Configuration["StratzApi:Token"] ?? string.Empty));
 
 builder.Services.AddSingleton<IStratzHeroParser, StratzHeroParser>();
@@ -39,30 +40,32 @@ builder.Services.AddSingleton<IHeroStatsAggregator, HeroStatsAggregator>();
 builder.Services.AddSingleton<IHeroInfoFormatter, HeroInfoFormatter>();
 
 // Регистрация основных процессоров
-builder.Services.AddSingleton<HeroStatsProcessor>();
-builder.Services.AddSingleton<HeroesDataService>();
+builder.Services.AddSingleton<IHeroStatsProcessor, HeroStatsProcessor>();
+builder.Services.AddSingleton<IHeroesDataService, HeroesDataService>();
+builder.Services.AddHostedService<HeroDataUpdateHostedService>();
 
 // Регистрация самого бота
 builder.Services.AddSingleton<Dota2MetaBot>();
 
-using IHost host = builder.Build();
+using var host = builder.Build();
 
 // 3. Запуск логики
 var bot = host.Services.GetRequiredService<Dota2MetaBot>();
 var cts = new CancellationTokenSource();
 
-Console.CancelKeyPress += (_, e) => {
+Console.CancelKeyPress += (_, e) =>
+{
     e.Cancel = true;
     cts.Cancel();
 };
 
 Console.WriteLine("Telegram бот запущен. Нажмите Ctrl+C для выхода.");
 
-try 
+try
 {
     await bot.StartReceivingAsync(cts.Token);
 }
-catch (OperationCanceledException) 
+catch (OperationCanceledException)
 {
     Console.WriteLine("Бот остановлен.");
 }
