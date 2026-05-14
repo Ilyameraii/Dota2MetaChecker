fe652510e7bbf29d11daac8e4b1bf0530922168e
Ilya Bobrov <ilyaraibusiness@gmail.com>
2026-05-14 12:36:43 +0300

    исправил баг с вычислением разницы рейтинга

--- Services/Processing/Extensions/HeroCalculatorExtensions.cs
+++ Services/Processing/Extensions/HeroCalculatorExtensions.cs
@@ -7,19 +7,19 @@ public static class HeroCalculatorExtensions
 {
     public static Hero WithWinrate(this Hero hero)
     {
-        var winRate = hero.MatchCount > 0 
+        var winRate = hero.MatchCount > 0
             ? (double)hero.WinCount / hero.MatchCount
             : 0;
-            
+
         return hero with { WinRate = winRate };
     }
-    
+
     public static Hero WithPickRate(this Hero hero, int totalMatchCount)
     {
-        var pickRate = totalMatchCount > 0 
-            ? (double)hero.MatchCount / totalMatchCount 
+        var pickRate = totalMatchCount > 0
+            ? (double)hero.MatchCount / totalMatchCount
             : 0;
-            
+
         return hero with { PickRate = pickRate };
     }
 
@@ -29,18 +29,37 @@ public static class HeroCalculatorExtensions
         {
             return hero with { Rating = double.MinValue };
         }
-        
+
         // формула рейтинга
         var rating = HeroRatingConstants.WinrateImpactValue * hero.WinRate + hero.PickRate;
-        
+
         return hero with { Rating = rating };
     }
 
     public static Hero WithDeltas(this Hero hero, Hero previous) =>
         hero with
         {
-            WinRateDelta = hero.WinRate - previous.WinRate,
-            PickRateDelta = hero.PickRate - previous.PickRate,
-            RatingDelta = hero.Rating - previous.Rating
+            WinRateDelta = CalculateWinrateDelta(hero, previous),
+            PickRateDelta = CalculatePickrateDelta(hero,previous),
+            RatingDelta = CalculateRatingDelta(hero, previous)
         };
+
+    private static double CalculateWinrateDelta(Hero hero , Hero previous) => hero.WinRate - previous.WinRate;
+    
+    private static double CalculatePickrateDelta(Hero hero, Hero previous)=> hero.PickRate - previous.PickRate;
+    
+    private static double CalculateRatingDelta(Hero hero, Hero previous)
+    {
+        if (hero.PickRate < HeroRatingConstants.MinPickrateForRating ||
+            previous.PickRate < HeroRatingConstants.MinPickrateForRating)
+        {
+            return double.MinValue;
+        }
+
+        // формула рейтинга
+        var delta = hero.Rating - previous.Rating;
+
+        return delta;
+    }
+
 }
